dd38d536454db0ed9ae667f35c325d4cdc22f81d
NafisMahim <mayflier123@gmail.com>
2025-06-13 22:57:51 -0400

    Update 20250611204055_polished_credit.sql

--- supabase/migrations/20250611204055_polished_credit.sql
+++ supabase/migrations/20250611204055_polished_credit.sql
@@ -1,21 +1,23 @@
--- polished_credit.sql
-
--- Create view for subscription lookup
+-- Drop the old view
 DROP VIEW IF EXISTS stripe_user_subscriptions CASCADE;
 
+-- Recreate the correct view
 CREATE OR REPLACE VIEW stripe_user_subscriptions AS
 SELECT
-  sc.user_id AS customer_id,
-  ss.id AS subscription_id,
-  ss.status AS subscription_status,
-  ss.price_id
+  sc.user_id,
+  ss.stripe_subscription_id,
+  ss.status,
+  ss.price_id,
+  ss.current_period_start,
+  ss.current_period_end,
+  ss.cancel_at_period_end
 FROM
   stripe_customers sc
-  LEFT JOIN stripe_subscriptions ss ON ss.customer_id = sc.customer_id
+LEFT JOIN stripe_subscriptions ss ON ss.user_id = sc.user_id
 WHERE
   sc.deleted_at IS NULL
-  AND ss.deleted_at IS NULL;
+  AND (ss.deleted_at IS NULL OR ss.deleted_at IS NULL);
 
--- Grant view access
+-- Re-grant access
 GRANT SELECT ON stripe_user_subscriptions TO authenticated;
 GRANT SELECT ON stripe_user_subscriptions TO service_role;
